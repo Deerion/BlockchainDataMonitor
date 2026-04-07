07d5cae23a9b8bdfc4906e65ad58468f380de7ad
Hubert <jaroszhubert12@gmail.com>
2026-04-07 11:41:23 +0200

    Konfiguracja wtyczki JaCoCo do raportowania pokrycia kodu.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    void simpleTest() {
+        // Ten test po prostu sprawdza, czy prawda jest prawdą.
+        // Wystarczy, aby "obudzić" JaCoCo.
+        assertTrue(true);
+    }
+}
