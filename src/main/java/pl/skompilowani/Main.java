58cc5844b9394ef41c7fdc590a6d0734c56424d2
Hubert <jaroszhubert12@gmail.com>
2026-04-07 12:03:50 +0200

    Rejestracja i konfiguracja kluczy API w usłudze Alchemy

--- src/main/java/pl/skompilowani/Main.java
+++ src/main/java/pl/skompilowani/Main.java
@@ -1,17 +1,38 @@
 package pl.skompilowani;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.math.BigInteger;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    // Inicjalizacja loggera dla klasy Main
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Uruchamianie Monitora Danych Blockchain...");
+
+        Dotenv dotenv = Dotenv.load();
+        String url = dotenv.get("BLOCKCHAIN_URL");
+
+        if (url == null || url.isEmpty()) {
+            logger.error("BŁĄD: Nie znaleziono zmiennej BLOCKCHAIN_URL w pliku .env!");
+            return;
+        }
+
+        BlockchainClient client = new BlockchainClient(url);
+
+        try {
+            logger.info("Próba połączenia z siecią testową Sepolia...");
+            BigInteger latestNum = client.getLatestBlockNumber();
+            logger.info("Sukces! Najnowszy numer bloku: {}", latestNum);
+
+            var block = client.getBlockDetails(latestNum);
+            logger.info("Dane bloku -> Hash: {}, Liczba transakcji: {}",
+                    block.getHash(), block.getTransactions().size());
+
+        } catch (Exception e) {
+            logger.error("Wystąpił błąd podczas komunikacji z blockchainem: ", e);
         }
     }
 }
