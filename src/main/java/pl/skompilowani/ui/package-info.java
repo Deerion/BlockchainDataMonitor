1c10c18ddc6b0e3c5c463c6581dbbb420e398465
Hubert <jaroszhubert12@gmail.com>
2026-05-04 21:17:46 +0200

    Implementacja walidacji wejścia użytkownika oraz testów (#44)

--- src/main/java/pl/skompilowani/ui/package-info.java
+++
@@ -1,7 +0,0 @@
-package pl.skompilowani.ui;
-
-/**
- * Warstwa Prezentacji (Presentation Tier).
- * Docelowe miejsce na klasy odpowiedzialne za wyświetlanie informacji użytkownikowi
- * (np. dedykowane menu w konsoli).
- */
